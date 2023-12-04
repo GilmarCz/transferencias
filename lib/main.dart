b69046c29f39d88144f122de19edc25d1e0808d6
GilmarCz <gilmarczeika@gmail.com>
2023-12-04 15:21:15 -0300

    ajustado o appBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class Transfer {
   double value;
-  DateTime date;
+  int recipientNumber;
 
-  Transfer(this.value, this.date);
+  Transfer(this.value, this.recipientNumber);
 }
 
 class TransfersList {
@@ -12,12 +12,10 @@ class TransfersList {
 
   TransfersList()
       : transfers = [
-          Transfer(450000000.0, DateTime.now()),
-          Transfer(436.0, DateTime.now().subtract(Duration(days: 1))),
-          Transfer(8001.0, DateTime.now().subtract(Duration(days: 2))),
-          Transfer(1234.0, DateTime.now().subtract(Duration(days: 3))),
-          Transfer(999998.0, DateTime.now().subtract(Duration(days: 4))),
-        ];
+    Transfer(450000000.0, 450),
+    Transfer(8001.0, 1234),
+    Transfer(999998.0, 1111),
+  ];
 
   void addTransfer(Transfer newTransfer) {
     transfers.add(newTransfer);
@@ -27,50 +25,56 @@ class TransfersList {
     transfers.remove(transferToRemove);
   }
 
-  // Outros métodos podem ser adicionados conforme necessário, como pesquisar, editar, etc.
+// Outros métodos podem ser adicionados conforme necessário, como pesquisar, editar, etc.
 }
 
 class TransfersPage extends StatelessWidget {
   final TransfersList transfersList = TransfersList();
 
+  TransfersPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Transferências"),
-      ),
       body: ListView.builder(
         itemCount: transfersList.transfers.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Column(
-              children: [
-                ListTile(
-                  contentPadding: EdgeInsets.all(
-                      0.0), // Remove o padding padrão do ListTile
-                  leading: CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 165, 165, 165),
-                    radius: 12.0, // Ajuste o valor conforme necessário
-                    child: Icon(
-                      Icons.attach_money,
-                      color: Colors.white,
-                      size: 18.0, // Ajuste o valor conforme necessário
-                    ),
+          return Column(
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.all(0.0),
+                leading: const CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 165, 165, 165),
+                  radius: 12.0,
+                  child: Icon(
+                    Icons.attach_money,
+                    color: Colors.white,
+                    size: 18.0,
                   ),
-                  title: Text(
-                      "${transfersList.transfers[index].value.toString()}"),
-                  subtitle: Text(
-                      transfersList.transfers[index].date.toIso8601String()),
                 ),
-                Divider(
-                    height: 1.5,
-                    color: Color.fromARGB(255, 202, 202,
-                        202)), // Adiciona a borda após cada linha
-              ],
-            ),
+                title: Text(
+                  "R\$ ${transfersList.transfers[index].value.toStringAsFixed(2)}",
+                ),
+                subtitle: Text(
+                  "${transfersList.transfers[index].recipientNumber}",
+                ),
+              ),
+              const Divider(
+                height: 1.5,
+                color: Color.fromARGB(255, 202, 202, 202),
+              ),
+            ],
           );
         },
+      ),
+      appBar: AppBar(
+        title: const Text("Transferências"),
+        backgroundColor: Colors.blue, // Define a cor de fundo da AppBar
+        iconTheme: const IconThemeData(color: Colors.white), toolbarTextStyle: const TextTheme(
+          titleLarge: TextStyle(color: Colors.white), // Define a cor do texto do título
+        ).bodyMedium, titleTextStyle: const TextTheme(
+          titleLarge: TextStyle(color: Colors.white), // Define a cor do texto do título
+        ).titleLarge,
       ),
     );
   }
